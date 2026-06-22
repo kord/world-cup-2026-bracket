52663c8ffc8eaa2fd023f3f625bcedecbe550088
Dave Kordalewski <davekordalewski@gmail.com>
2026-06-22 10:23:19 +0800

    reorder knockout matches some more

--- src/components/KnockoutBracket.tsx
+++ src/components/KnockoutBracket.tsx
@@ -17,8 +17,8 @@ function isPlaceholder(name: string): boolean {
 const LEFT_R32 = [73, 75, 74, 77, 76, 78, 79, 80];
 const LEFT_R16 = [90, 89, 91, 92];
 const LEFT_QF = [97, 99];
-const RIGHT_R32 = [82, 81, 83, 84, 88, 86, 85, 87];
-const RIGHT_R16 = [94, 93, 95, 96];
+const RIGHT_R32 = [82, 81, 84, 83, 85, 87, 88, 86];
+const RIGHT_R16 = [94, 93, 96, 95];
 const RIGHT_QF = [98, 100];
 const CENTER_IDS = [101, 103, 104, 102]; // SF1, 3rd, Final, SF2
 
